7d24845fffd79944b840fe12db646fdc97ede846
Gergely Nagy <algernon@keyboard.io>
2021-10-17 21:12:29 +0200

    wip: mouse stuff
    Signed-off-by: Gergely Nagy <algernon@keyboard.io>

--- STM32F1/libraries/USBComposite/Mouse.cpp
+++ STM32F1/libraries/USBComposite/Mouse.cpp
@@ -13,9 +13,9 @@ void HIDMouse::end(void){
 void HIDMouse::click(uint8_t b)
 {
 	_buttons = b;
-	move(0,0,0);
+  sendReport();
 	_buttons = 0;
-	move(0,0,0);
+  sendReport();
 }
 
 void HIDMouse::move(signed char x, signed char y, signed char wheel)
@@ -24,16 +24,23 @@ void HIDMouse::move(signed char x, signed char y, signed char wheel)
 	reportBuffer[2] = x;
 	reportBuffer[3] = y;
 	reportBuffer[4] = wheel;
+}
 
-    sendReport();
+void HIDMouse::stop(bool x, bool y, bool wheel)
+{
+  if (x)
+    reportBuffer[2] = 0;
+  if (y)
+    reportBuffer[3] = 0;
+  if (wheel)
+    reportBuffer[4] = 0;
 }
 
 void HIDMouse::buttons(uint8_t b)
 {
 	if (b != _buttons)
 	{
-        _buttons = b;
-		move(0,0,0);
+    _buttons = b;
 	}
 }
 
@@ -47,10 +54,14 @@ void HIDMouse::release(uint8_t b)
 	buttons(_buttons & ~b);
 }
 
+void HIDMouse::releaseAll()
+{
+  _buttons = 0;
+}
+
 bool HIDMouse::isPressed(uint8_t b)
 {
 	if ((b & _buttons) > 0)
 		return true;
 	return false;
 }
-
